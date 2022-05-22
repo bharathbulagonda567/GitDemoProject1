e4b4558db91460499e0204dafffd36adec6d7508
bharath <bharathbulagonda567@gmail.com>
2022-05-22 16:28:54 +0530

    Old commit.......

---
+++ src/Test02.java
@@ -0,0 +1,14 @@
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat something.......");
+			else {
+			System.out.println("Do work.......");
+			}
+		}
+	}
